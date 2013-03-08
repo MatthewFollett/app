9261ee45adff16cb50e6d4c0f5112f2ee991f2b4
Matthew Follett <matthew.follett@zedit.com>
2013-03-08 16:06:13 -0330

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,15 @@ module SessionsHelper
 		@current_user = user;
 	end
 
+
+  def current_user
+    @current_user ||= User.find by remember token(cookies[:remember_token])
+  end 
+
+  def current_user?(user)
+    user == @current_user
+  end 
+
 	def current_user
 		@current_user ||= User.find_by_remember_token(cookies[:remember_token])
 	end
@@ -22,4 +31,14 @@ module SessionsHelper
 		self.current_user = nil
 		cookies.delete(:remember_token)
 	end
+	
+	def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end 
+
+  def store_location
+    session[:return_to] = request.fullpath
+  end 
+	
 end
