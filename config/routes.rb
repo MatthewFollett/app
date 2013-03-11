4c18cb96d7d152dacfe1f16dc0f6afaedfe09c3c
Matthew Follett <matthew.follett@zedit.com>
2013-03-11 14:57:47 -0230

    Add user Microposts

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ SampleApp::Application.routes.draw do
 
   root to: 'static_page#home'
 	resources :sessions, only: [:new, :create, :destroy, :get]
+	resources :microposts, only: [:create, :destroy]
 
   match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
